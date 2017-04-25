1b0fd34bbfc4b0077ff49f483e955e127436cb64
andrewliu19 <andrewliu274@gmail.com>
2017-04-25 09:26:02 -0700

    Submit form button

--- subsites/designer/javascript.js
+++ subsites/designer/javascript.js
@@ -1,3 +1,6 @@
 function myFunction() {
    document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
+    function formSubmit() {
+        google.script.run.emailTech(document.forms[0]);
+    }
